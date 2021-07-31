26ea8480937138874f58ee6507a3b1ddc83c1028
sonofevil <debilerpc@web.de>
2021-07-31 14:06:33 +0200

    cleanup commit 1

--- metalarchives_importer.user.js
+++ metalarchives_importer.user.js
@@ -111,93 +111,6 @@ function retrieveReleaseInfo(release_url) {
 
     release = setreleasedate(release, rdata['Release date']);
     
-    /*if ('Label' in rdata) {
-        // TODO: add case for multiple labels if such a case exist
-        let label = rdata['Label'];
-        let label_mbid = '';
-        if (label == 'Independent') {
-            label = '[no label]';
-            label_mbid = '157afde4-4bf5-4039-8ad2-5a15acc85176';
-        }
-        let catno = rdata['Catalog ID'];
-        if (catno == undefined || catno == 'N/A') {
-            catno = '';
-        }
-        release.labels.push({
-            name: label,
-            catno: catno,
-            mbid: label_mbid,
-        });
-    }*/
-
-    /*if (rdata['Type'] in ReleaseTypes) {
-        let types = ReleaseTypes[rdata['Type']];
-        release.type = types[0];
-        // NOTE: secondary type may not be selected on MB editor, but it still works, a bug on MB side
-        release.secondary_types = types.slice(1);
-    }*/
-
-    // FIXME: multiple vinyls ie. http://www.metal-archives.com/albums/Reverend_Bizarre/III%3A_So_Long_Suckers/415313
-    /*if (rdata['Format'] in ReleaseFormat) {
-        release.format = ReleaseFormat[rdata['Format']];
-    }
-
-    if ('Version desc.' in rdata) {
-        if (rdata['Version desc.'].indexOf('Digipak') != -1) {
-            release.packaging = 'Digipak';
-        }
-        if (release.format == 'CD' && rdata['Version desc.'] == 'CD-R') {
-            release.format = 'CD-R';
-        }
-    }
-
-    let identifiers = $('#album_tabs_notes > div:nth-child(2)').find('p:not([class])').contents();
-    
-    for (let j = 0; j < identifiers.length; j++) {
-        if (identifiers[j].textContent.indexOf('Barcode:') != -1) {
-            release.barcode = $.trim(identifiers[j].textContent.substring(8));
-            break;
-        }
-    }
-
-    // URLs
-    let link_type = MBImport.URL_TYPES;
-    
-    release.urls.push({
-        url: release_url,
-        link_type: link_type.other_databases,
-    });
-
-    let releaseNumber = 0;
-    
-    let disc = {
-        tracks: [],
-        format: release.format,
-    };
-    
-    release.discs.push(disc);
-
-    let tracksline = $('table.table_lyrics tr.even,table.table_lyrics tr.odd');
-
-    tracksline.each(function (index, element) {
-        let trackNumber = $.trim(element.children[0].textContent).replace('.', '');
-        if (trackNumber == '1' && trackNumber != index + 1) {
-            releaseNumber++;
-            release.discs.push({
-                tracks: [],
-                format: release.format,
-            });
-        }
-
-        // TODO: handling of split and compilation artists (artist - title)
-        let track = {
-            number: trackNumber,
-            title: $.trim(element.children[1].textContent.replace(/\s+/g, ' ')),
-            duration: $.trim(element.children[2].textContent),
-            artist_credit: [release.artist_credit],
-        };
-        release.discs[releaseNumber].tracks.push(track);
-    });*/
     return release;
 }
 
@@ -224,25 +137,6 @@ function insertLink(release, release_url) {
     mbUI.slideDown();
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                   Metal Archives -> MusicBrainz mapping                                                   //
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-release.type 	primary type release 	secondary release type
-on MA				on MB
-Full-length 		Album				Compilation
-Live album			Single				Demo
-Demo				EP					DJ-mix
-Single				Broadcast			Interview
-EP					Other				Live
-Video									Audiobook
-Boxed set								Mixtape/Street
-Split									Remix
-Video/VHS (legacy)						Soundtrack
-Compilation								Spokenword
-Split video
-*/
-
 //ReleaseTypes[MAtype]=["primary type","secondary type on mb"];
 var ReleaseTypes = {
     'Full-length': ['album'],
@@ -254,19 +148,3 @@ var ReleaseTypes = {
     Split: ['album'],
     Collaboration: [''],
 };
-
-//ReleaseFormat[MAformat]="MBformat";
-/*var ReleaseFormat = {
-    CD: 'CD',
-    '2CD': 'CD',
-    Vinyl: 'Vinyl',
-    '7" vinyl': '7" Vinyl',
-    '7" vinyl (33⅓ RPM)': '7" Vinyl',
-    '10" vinyl (33⅓ RPM)': '10" Vinyl',
-    '10" vinyl': '10" Vinyl',
-    '12" vinyl': '12" Vinyl',
-    '2 12" vinyls': '12" Vinyl',
-    '12" vinyl (33⅓ RPM)': '12" Vinyl',
-    Cassette: 'Cassette',
-    Digital: 'Digital Media',
-};*/
